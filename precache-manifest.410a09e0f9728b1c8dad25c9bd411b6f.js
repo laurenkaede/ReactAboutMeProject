c527e95622a8e3d5bbc3a5c4cbde6e871114825b
Lauren Moor <laurenkaede@gmail.com>
2020-03-29 15:34:26 +0100

    Updates

---
+++ precache-manifest.410a09e0f9728b1c8dad25c9bd411b6f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b54652a3149612eb59600f3839f757df",
+    "url": "/ReactAboutMe/index.html"
+  },
+  {
+    "revision": "ce7811501ba01bbac55b",
+    "url": "/ReactAboutMe/static/css/main.89a40a4e.chunk.css"
+  },
+  {
+    "revision": "545a949e9468a1ab27ff",
+    "url": "/ReactAboutMe/static/js/2.d19dca30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactAboutMe/static/js/2.d19dca30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce7811501ba01bbac55b",
+    "url": "/ReactAboutMe/static/js/main.4fbab18b.chunk.js"
+  },
+  {
+    "revision": "895a2a7c717183897311",
+    "url": "/ReactAboutMe/static/js/runtime-main.44a66a5d.js"
+  },
+  {
+    "revision": "91f0324e98f4f9d0a0f51634137709d6",
+    "url": "/ReactAboutMe/static/media/bella.91f0324e.jpg"
+  },
+  {
+    "revision": "741b723acba1cb9f23744c454b59e52c",
+    "url": "/ReactAboutMe/static/media/daisy.741b723a.jpg"
+  }
+]);
